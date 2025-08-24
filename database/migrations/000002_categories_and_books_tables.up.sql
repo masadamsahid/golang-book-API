0f4831c0892b612be792646948a3c319b1ddac54
Adam Sahid <masadamsahid@gmail.com>
2025-08-24 08:30:12 +0700

    add categories and books tables and models

---
+++ database/migrations/000002_categories_and_books_tables.up.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS categories (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+  created_by VARCHAR NOT NULL,
+  modified_at TIMESTAMP WITH TIME ZONE,
+  modified_by VARCHAR
+);
+
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  description TEXT,
+  image_url VARCHAR,
+  release_year INTEGER NOT NULL,
+  price INTEGER,
+  total_page INTEGER,
+  thickness VARCHAR,
+  category_id INTEGER,
+  created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+  created_by VARCHAR NOT NULL,
+  modified_at TIMESTAMP WITH TIME ZONE,
+  modified_by VARCHAR,
+  CONSTRAINT fk_books_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
+);
